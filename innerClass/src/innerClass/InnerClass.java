814ad23a57d05c2d7b5f4a195c688015b5454d00
darsana-temp <darsana-temp@192.168.1.100>
2023-03-08 14:50:13 +0530

    Exercise - inner class

---
+++ innerClass/src/innerClass/InnerClass.java
@@ -0,0 +1,108 @@
+package innerClass;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+abstract //main class
+class Browser
+{
+	abstract void Shortcut();
+	//member inner class
+	class BookMarks
+	{
+		static ArrayList<String> bookMark = new ArrayList<>();
+		
+		//add a bookMark
+		void setBookMark(String url)
+		{
+			bookMark.add(url);
+		}
+		
+		//view bookMarks
+		void displayBookMarks()
+		{
+			System.out.println("BookMarks :");
+			for(String str:bookMark)
+				System.out.println(str);
+		}
+		
+	}
+	
+	
+	//static nested class
+	static class History
+	{
+		static ArrayList<String> history = new ArrayList<>();
+		
+		//set history
+		static void setHistory(String url)
+		{
+			history.add(url);
+		}
+		
+		//display history
+		static void displayHistory()
+		{
+			System.out.println("History : ");
+			for(String str:history)
+				System.out.println(str);
+		}
+	}
+
+}
+	
+public class InnerClass {
+
+	public static void main(String[] arg)
+	{
+		int count;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number of browsing :");
+		count=sc.nextInt();
+		ArrayList<String> shortcut = new ArrayList<>();
+		
+		Browser Browser1 = new Browser()
+		{
+			public void Shortcut()
+			{
+				System.out.println("Reloads your current page : ⌘ +R");
+				System.out.println("Opens the find baR : ⌘+F");
+				System.out.println("Prints your current page : ⌘+P");
+				System.out.println("Saves your current page : ⌘+S");
+			}
+		
+		};
+		
+		//display shortcut
+		System.out.println("shortcuts keys : ");
+		Browser1.Shortcut();
+		System.out.println();
+		for(int i=1;i<=count;i++)
+		{
+			
+			System.out.println("enter url");
+			String url = sc.next();
+			Browser.History.setHistory(url);
+			System.out.println();
+						
+			//bookMark
+			System.out.println("want to bookmark url");
+			Browser.BookMarks bookMark = Browser1.new BookMarks();
+			if(sc.nextBoolean())
+			{
+				bookMark.setBookMark(url);
+			}
+			System.out.println();
+			
+			//display history
+			Browser.History.displayHistory();
+			System.out.println();
+			
+			//display bookMarks
+			bookMark.displayBookMarks();
+			System.out.println();
+			
+		}
+		
+	}
+}
