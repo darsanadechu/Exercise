5dd253057863655bf3c4cf3bed7eac3ec089255a
darsana-temp <darsana-temp@192.168.1.100>
2023-03-09 19:32:31 +0530

    Exercise - Inner class

--- innerClass/src/innerClass/InnerClass.java
+++ innerClass/src/innerClass/InnerClass.java
@@ -55,7 +55,7 @@ public class InnerClass {
 
 	public static void main(String[] arg)
 	{
-		boolean choice;
+		boolean BookMarkChoice;
 		Scanner sc = new Scanner(System.in);
 		ArrayList<String> shortcut = new ArrayList<>();
 		
@@ -70,38 +70,56 @@ public class InnerClass {
 			}
 		
 		};
+		Browser.BookMarks bookMark = Browser1.new BookMarks();
 		
-		//display shortcut
-		System.out.println("shortcuts keys : ");
-		Browser1.Shortcut();
-		do
+		
+		for(int i=1;i<10;i++)
 		{
+			System.out.println("Menu :");
+			System.out.println("1: add urls");
+			System.out.println("2: display urls");
+			System.out.println("3: display bookmark");
+			System.out.println("4: display shortcut");
 			
-			System.out.println("enter url");
-			String url = sc.next();
-			Browser.History.setHistory(url);
-			System.out.println();
-						
-			//bookMark
-			System.out.println("want to bookmark url");
-			Browser.BookMarks bookMark = Browser1.new BookMarks();
-			if(sc.nextBoolean())
+			System.out.println("enter your choice ");
+			int choice=sc.nextInt();
+			switch(choice)
 			{
-				bookMark.setBookMark(url);
+			case 1: do
+					{
+						//history
+						System.out.println("enter url");
+						String url = sc.next();
+						Browser.History.setHistory(url);
+									
+						//bookMark
+						System.out.println("want to bookmark url");
+						if(sc.nextBoolean())
+						{
+							bookMark.setBookMark(url);
+						}
+						System.out.println("want to browse more ");
+						BookMarkChoice = sc.nextBoolean();
+					}while(BookMarkChoice);
+					break;
+					
+			case 2: //display history
+					Browser.History.displayHistory();
+					break;
+					
+			case 3: //display bookMarks
+					bookMark.displayBookMarks();
+					break;
+					
+			case 4: //display shortcut
+					System.out.println("shortcuts keys : ");
+					Browser1.Shortcut();
+					break;
+			default: System.out.println("invalid choice");
+					
+					
+			
 			}
-			System.out.println();
-			
-			//display history
-			Browser.History.displayHistory();
-			System.out.println();
-			
-			//display bookMarks
-			bookMark.displayBookMarks();
-			System.out.println();
-			
-			System.out.println("want to browse more ");
-			choice = sc.nextBoolean();
-		}while(choice);
-		
+		}
 	}
 }
